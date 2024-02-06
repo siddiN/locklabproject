104099c27cf7daf24b51724fe598398a65768fdb
siddiN <siddinalgonda10gmail.com>
2024-02-06 12:18:16 +0530

    first commit

---
+++ githubDemo/src/githubDemo/addingtwonumbers.java
@@ -0,0 +1,14 @@
+package githubDemo;
+
+public class addingtwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1= 5, num2 =6, sum;
+		sum= num1+num2;
+		System.out.println(""+sum);
+
+	}
+
+}
